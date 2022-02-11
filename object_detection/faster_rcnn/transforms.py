e7d50dde9833a9b0c8e6a544a89c16f4ed19c5f0
Fang Zhaoyan <2788321369@qq.com>
2022-02-11 23:18:08 +0800

    add pascal_voc dataset self --- dataset

---
+++ object_detection/faster_rcnn/transforms.py
@@ -0,0 +1,39 @@
+import random
+from torchvision.transforms import functional as F
+
+
+class Compose(object):
+    """组合多个transform函数"""
+
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, image, target):
+        for t in self.transforms:
+            image, target = t(image, target)
+        return image, target
+
+
+class ToTensor(object):
+    """将PIL图像转成Tensor"""
+
+    def __call__(self, image, target):
+        image = F.to_tensor(image)
+        return image, target
+
+
+class RandomHorizontalFlip(object):
+    """随机水平翻转图像 以及boundingBoxes"""
+
+    def __init__(self, prob=0.5):
+        self.prob = prob
+
+    def __call__(self, image, target):
+        if random.random() < self.prob:
+            height, width = image.shape[-2:]
+            image = image.flip(-1)  # 水平翻转图片
+            bbox = target["boxes"]
+            # bbox: xmin ymin xmax ymax 水平翻转y不变
+            bbox[:, [0, 2]] = width - bbox[:, [2, 0]]
+            target["boxes"] = bbox
+        return image, target
